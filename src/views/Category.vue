d7301a4b3e761375b0fd4893b9ca18d9c61fd125
wangzp <6010625@qq.com>
2019-06-15 18:47:56 +0800

    删除弹窗

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -5,41 +5,22 @@
 			<!-- <router-link v-for="router in routerList"></router-link> -->
 		</nav>
 		<router-view></router-view>
-		<custom-dialog :options="dialogOption" @yesFun="success" @noFun="hideDialog">
-			<p slot="dialog-content">法拉涉及到法律就撒地方事件砥砺奋进士大夫吉林省的两室就得傅雷家书多了几分两室就得了圣诞节福利熟练度 塑料袋积分</p>
-		</custom-dialog>
-		<button @click="showDialog">弹窗</button>
 	</div>
 </template>
 
 <script>
-	import CustomDialog from '../dialog/custom-dialog'
 
 export default {
 	data() {
 		return {
 			navList:[],
-			dialogOption: {
-				title: "I'm title",
-				yesText: "yes",
-				noText: "no",
-				isShow: false
-			}
 		};
 	},
 	components: {
-		CustomDialog
+		
 	},
 	methods: {
-		showDialog() {
-			this.dialogOption.isShow = true;
-		},
-		hideDialog() {
-			this.dialogOption.isShow = false;
-		},
-		success() {
-			alert("success");
-		}
+		
 	}
 }
 </script>
