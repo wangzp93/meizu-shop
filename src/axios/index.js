3a872efc6ac8eb3fa90da7ae62745502fc37c812
wangzhongpeng <6010625@qq.com>
2019-08-23 11:17:44 +0800

    axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = function(obj, successCallback, failCallback) {
+// axios.defaults.baseURL = 'https://easy-mock.com/mock/5d1b63a94e718016e83ba630/meizu-shop'
+
+Vue.prototype.$axios = function (obj, successCallback, failCallback) {
   axios({
+      method: 'post',
+      baseURL: 'https://easy-mock.com/mock/5d1b63a94e718016e83ba630/meizu-shop',
+      transformRequest: [function (data) {
+          // 可以在此处加loading
+          
+          return data;
+      }],
       ...obj
   }).then( response => {
       if (typeof successCallback === 'function') {
@@ -18,4 +27,3 @@ Vue.prototype.$axios = function(obj, successCallback, failCallback) {
   });
 }
 
-axios.defaults.baseURL = 'https://easy-mock.com/mock/5d1b63a94e718016e83ba630/meizu-shop'
