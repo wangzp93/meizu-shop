14ff616f20d2e665d362583b6090f2604c180c57
wangzp <6010625@qq.com>
2020-03-29 23:33:26 +0800

    axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,48 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+let $axios = axios.create({
+    method: 'post',
+    baseURL: process.env.BASE_API,
+    // timeout: 1000,
+    // "Content-Type": "application/json",
+    transformRequest: [function (data) {
+        if (typeof data === "object") {
+            let str = "", i = 0;
+            for (let key in data) {
+                if (i++ != 0) {
+                    str += "&";
+                }
+                str += key + "=" + data[key];
+            }
+            return str;
+        }
+        return data;
+    }],
+});
+$axios.interceptors.response.use(function (res) {
+    if (res.status === 200) {
+        return res.data;
+    }
+    return res;
+}, function (err) {
+    if (err.request) {
+        let req = err.request;
+        // easy-mock失败，请求本地json
+        let config = err.config;
+        config.url = config.url.replace(config.baseURL, "static/json/") + ".json";
+        config.baseURL = "";
+        config.method = "get";
+        return $axios(config);
+    }
+    return Promise.reject(err);
+});
+Object.defineProperty(Vue.prototype, "$axios", {
+    value: $axios
+});
 
 // 返回结果拦截器
-axios.interceptors.response.use(function (res) {
+/* axios.interceptors.response.use(function (res) {
     if (res.status === 200) {
         return res.data;
     }
@@ -20,9 +59,6 @@ axios.interceptors.response.use(function (res) {
     }
     // return Promise.reject(err);
 });
-
-// axios.defaults.baseURL = 'https://easy-mock.com/mock/5d1b63a94e718016e83ba630/meizu-shop'
-
 Vue.prototype.$axios = function(obj) {
     // 根据不同环境取不同地址
     const baseURL = process.env.BASE_API;
@@ -35,4 +71,4 @@ Vue.prototype.$axios = function(obj) {
         }],
         ...obj
     });
-}
+} */
