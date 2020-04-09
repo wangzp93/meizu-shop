0cdc5dd250e03f97fc7c57a8dd72a5680ca1766e
wangzp <6010625@qq.com>
2020-04-09 23:36:06 +0800

    up

--- build/utils.js
+++ build/utils.js
@@ -48,7 +48,7 @@ exports.cssLoaders = function (options) {
       return ExtractTextPlugin.extract({
         use: loaders,
         fallback: 'vue-style-loader',
-        publicPath: '../../'        //添加该行（解释：pubicpath为打包后app.css至index.html的相对路径）
+        publicPath: '../../'        //添加该行（解释：publicpath为打包后app.css至index.html的相对路径）
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
